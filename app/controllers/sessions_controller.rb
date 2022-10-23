d7bac2b0d5adb3f99c833fa855ab980415f3d8c1
Pete Fowler <fowler.pete@gmail.com>
2022-10-23 13:08:11 -0600

    Add sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  def create 
+    user = User.find_by(params[:username])
+    if user&.authenticate 
+      session[:user_id] = user.id 
+      render json: user, status: :created
+    else
+      render json: {error: 'Invalid username or password'}, status: :unauthorized
+    end
+  end
+
+  def destroy
+
+  end 
 end
