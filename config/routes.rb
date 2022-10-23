73a8f10a3b161e55cd678be0f2f540b7e8537eeb
Pete Fowler <fowler.pete@gmail.com>
2022-10-23 13:00:54 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :cities
   resources :users
 
+  post '/login', to:'sessions#create'
+  delete '/destroy', to: 'sessions#destroy'
+  
+  get '/me', to:'users#show'
+  post '/signup', to: 'users#create'
 end
