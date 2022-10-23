a6acaf54eb96531ae8ea7aaaab10686f91ec0944
Tim <dalytimdaly@gmail.com>
2022-10-23 13:07:40 -0600

    Branch created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    # POST /users - SIGNUP
+  def create
+    user = User.create!(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
