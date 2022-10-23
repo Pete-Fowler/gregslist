94c8fe49dff9559e3008fc8e21317324afa15262
Pete Fowler <fowler.pete@gmail.com>
2022-10-23 15:30:39 -0600

    Add login component structure
    Begin first form

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,14 @@
+import styles from './Login.module.css';
 
 export default function Login() {
 
   return (
-    <div classNmae='login'>
-      Login
+    <div className={styles.loginBox}>
+      <form className={styles.login}>
+        <div className={styles.loginTitle}>Log in</div>
+      </form>
+      <p>or</p>
+      <div className={styles.createAccount}>create an account</div>
     </div>
   )
 }
