012c075f4fc37f5a3c0fe7bee188292a13802d35
Pete Fowler <fowler.pete@gmail.com>
2022-10-23 12:56:15 -0600

    Add sessions controller, user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :posts
     has_many :events
     has_many :cities, through: :posts  
     has_many :cities, through: :events
+    validates :username, presence: true, uniqueness: true
 end
