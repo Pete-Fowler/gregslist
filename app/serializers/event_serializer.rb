6ff0ce52264d198ed4be449a8655ef448c4eb07b
Tim <dalytimdaly@gmail.com>
2022-10-24 12:04:39 -0600

    Database progress

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :city_id, :title, :date, :duration, :description, :image, :category, :area, :postal_code, :price, :created_at, :updated_at
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :city_id, :title, :description, :image, :category, :subcategory, :area, :postal_code, :price, :created_at, :updated_at
 end
