ca45bd85c78b63450f85442ff0bcefe84c5d6d7f
Pete Fowler <fowler.pete@gmail.com>
2022-10-24 09:31:25 -0600

    Start login controlled forms

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,13 +3,26 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../Home/Home.js';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [ user, setUser ] = useState(null);
+
+  useEffect(() => {
+    fetch(`/me`)
+    .then(res => res.json())
+    .then(data => setUser(data));
+  }, [])
+
+  function newUser(userData) {
+    setUser(userData);
+  }
+
   return (
     <div className={styles.app}>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login user={user} newUser={newUser}/>} />
       </Routes>
       <Footer />
     </div>
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,24 @@
 import styles from './Login.module.css';
+import { useState } from 'react';
 
-export default function Login() {
+export default function Login({ user, newUser }) {
+  const [ createData, setCreateData] = useState({
+    username: '',
+    password: '',
+    password_confirmation: ''
+  });
+  const [ loginData, setLoginData ] = useState({
+    username: '',
+    password: ''
+  })
+
+  function loginFormChange(e) {
+    e.
+  }
+
+  function handleCreate(e) {
+    e.preventDefault();
+  }
 
   return (
     <div className={styles.loginBox}>
@@ -8,11 +26,11 @@ export default function Login() {
         <h1 className={styles.loginTitle}>Log in</h1>
         <div className={styles.field}>
           <label>Email / Handle</label>
-          <input type='email'></input>
+          <input type='email' value={loginData.username} onChange={(e) => setLoginData({...loginData, username: e.target.value})}></input>
         </div>
-        <div className={styles.field}>
+        <div className={styles.field} >
           <label>Password</label>
-          <input type='text'></input>
+          <input type='text' value={loginData.password} onChange={loginFormChange}></input>
         </div>
         <button type='submit' className={styles.loginBtn}>Log in</button>
       </form>
@@ -23,7 +41,15 @@ export default function Login() {
           <label>Email</label>
           <input type='email'></input>
         </div>
-        <button type='submit' className={styles.loginBtn}>Create account</button>
+        <div className={styles.field}>
+          <label>Password</label>
+          <input type='text'></input>
+        </div>
+        <div className={styles.field}>
+          <label>Password Confirmation</label>
+          <input type='text'></input>
+        </div>
+        <button type='submit' className={styles.loginBtn} onSubmit={handleCreate}>Create account</button>
       </form>
     </div>
   )
