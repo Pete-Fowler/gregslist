f5f707e8f145dcbe4eccdc26df410467ba8895ee
Pete Fowler <fowler.pete@gmail.com>
2022-10-24 10:51:29 -0600

    Remove react-app boilerplate

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -8,11 +8,11 @@ import { useState, useEffect } from 'react';
 function App() {
   const [ user, setUser ] = useState(null);
 
-  useEffect(() => {
-    fetch(`/me`)
-    .then(res => res.json())
-    .then(data => setUser(data));
-  }, [])
+  // useEffect(() => {
+  //   fetch(`/me`)
+  //   .then(res => res.json())
+  //   .then(data => setUser(data));
+  // }, [])
 
   function newUser(userData) {
     setUser(userData);
