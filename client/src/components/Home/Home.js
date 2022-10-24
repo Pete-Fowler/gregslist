d13830aca456dfd1099dd3e3aca096672fbd6838
Pete Fowler <fowler.pete@gmail.com>
2022-10-24 15:34:26 -0600

    Add header, increase center width

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,13 +7,20 @@ export default function Home({ user }) {
 
   return (
     <div className={styles.home}>
+      {/* ================LEFT BAR============ */}
       <div className={styles.leftBar}>
         <Link className={styles.siteTitle}>gregslist</Link>
         <br></br>
         <Link className={styles.posting}>create a posting</Link>
         <Link to={path}>my account</Link>  
       </div>
-      <div className={styles.main}>Main</div>
+      {/* ==========MAIN / CENTER============= */}
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <Link>denver, CO</Link>
+        </div>
+      </div>
+      {/* ============RIGHT BAR================ */}
       <div className={styles.rightBar}>Right bar</div>
     </div>
   )
