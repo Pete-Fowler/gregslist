8b72eb9a581b477af2c099a0bb405934b2f3322c
Pete Fowler <fowler.pete@gmail.com>
2022-10-24 15:56:08 -0600

    Add subhead link style

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -19,9 +19,39 @@ export default function Home({ user }) {
         <div className={styles.header}>
           <Link>denver, CO</Link>
         </div>
+        <div className={styles.col1}>
+          <div className={styles.section}>
+            <div className={styles.subhead}><Link>community</Link></div>
+            {communityContent}
+          </div>
+        </div>
       </div>
       {/* ============RIGHT BAR================ */}
       <div className={styles.rightBar}>Right bar</div>
+    </div>
+  )
+}
+
+function communityContent() {
+  return (
+    <div className={styles.sectionContent}>
+      <Link>activities</Link>
+      <Link>artists</Link>
+      <Link>childcare</Link>
+      <Link>classes</Link>
+      <Link>events</Link>
+      <Link>general</Link>
+      <Link>groups</Link>
+      <Link>local news</Link>
+      <Link>lost+found</Link>
+      <Link>missed connections</Link>
+      <Link>musicians</Link>
+      <Link>pets</Link>
+      <Link>politics</Link>
+      <Link>rants & raves</Link>
+      <Link>rideshare</Link>
+      <Link>volunteers</Link>
+      <Link>services</Link>
     </div>
   )
 }
