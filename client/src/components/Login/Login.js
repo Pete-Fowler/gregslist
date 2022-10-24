334034c31225b74ab3fd8eaca604437b38e7b536
Pete Fowler <fowler.pete@gmail.com>
2022-10-24 09:59:39 -0600

    Add conditional render to login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -13,24 +13,39 @@ export default function Login({ user, newUser }) {
   })
 
   function loginFormChange(e) {
-    e.
+    setLoginData({...loginData, [e.target.name]: e.target.value});
+  }
+
+  function createFormChange(e) {
+    setCreateData({...createData, [e.target.name]: e.target.value})
   }
 
   function handleCreate(e) {
     e.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(createData)
+    })
+    .then(res => res.json())
+    .then(data => newUser(data));
   }
 
+  
   return (
-    <div className={styles.loginBox}>
+    user ? <h1>my account</h1> 
+    : <div className={styles.loginBox}>
       <form className={styles.login}>
         <h1 className={styles.loginTitle}>Log in</h1>
         <div className={styles.field}>
           <label>Email / Handle</label>
-          <input type='email' value={loginData.username} onChange={(e) => setLoginData({...loginData, username: e.target.value})}></input>
+          <input type='email' name='email' value={loginData.username} onChange={loginFormChange}></input>
         </div>
         <div className={styles.field} >
           <label>Password</label>
-          <input type='text' value={loginData.password} onChange={loginFormChange}></input>
+          <input type='text' name='password'value={loginData.password} onChange={loginFormChange}></input>
         </div>
         <button type='submit' className={styles.loginBtn}>Log in</button>
       </form>
@@ -39,15 +54,15 @@ export default function Login({ user, newUser }) {
         <h1 className={styles.createTitle}>Create an account</h1>
         <div className={styles.field}>
           <label>Email</label>
-          <input type='email'></input>
+          <input type='email' name='email' onChange={createFormChange}></input>
         </div>
         <div className={styles.field}>
           <label>Password</label>
-          <input type='text'></input>
+          <input type='text' name='password' onChange={createFormChange}></input>
         </div>
         <div className={styles.field}>
           <label>Password Confirmation</label>
-          <input type='text'></input>
+          <input type='text' name='password_confirmation' onChange={createFormChange}></input>
         </div>
         <button type='submit' className={styles.loginBtn} onSubmit={handleCreate}>Create account</button>
       </form>
