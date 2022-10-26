4b249b9d1901d2e00ea06fda15e18ba8e80570ed
Pete Fowler <fowler.pete@gmail.com>
2022-10-26 14:34:44 -0600

    Add created/updated metadata to post details

--- client/src/components/Post/PostDetails.js
+++ client/src/components/Post/PostDetails.js
@@ -1,6 +1,7 @@
 import styles from './PostDetails.module.css';
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom'
+import { formatDistance } from 'date-fns';
 
 export default function PostDetails() {
 
@@ -9,6 +10,8 @@ export default function PostDetails() {
   const { id } = useParams();
 
   const navigate = useNavigate();
+
+  const today = new Date();
 
   useEffect(() => {
     fetch(`/posts/${id}`)
@@ -21,19 +24,25 @@ export default function PostDetails() {
     });
   }, [id])
 
-  
   return (
     <div className={styles.postBox}>
-      <h3>{post.title} - {post.price} <span className={styles.area}>({post.area})</span></h3>
+      <h3>{post.title} - ${post.price} <span className={styles.area}>({post.area})</span></h3>
       <img src={post.image} alt="item" width="400" height="400"/>
-      <p>{post.category}</p>
-      <p>{post.price}</p>
-      <p>{post.area}</p>
-      <p>{post.postal_code}</p>
       <p>{post.description}</p>
-      <p>{post.user_id}</p>
-      <p>{post.city_id}</p>
-      <button onClick={()=>{navigate(`/posts`)}}>return to all posts</button>
+      <p className={styles.indent}>• do NOT contact me with unsolicited services or offers</p>
+      <div className={styles.metadata}>
+        <span>post id: {post.id}</span>
+        <span>posted: {post.created_at
+          ? <span className={styles.underlined}>{formatDistance(new Date(post.created_at), today)}
+          </span>
+          : null} ago
+        </span>
+        <span>updated: {post.updated_at
+          ? <span className={styles.underlined}>{formatDistance(new Date(post.created_at), today)}
+          </span>
+          : null} ago
+        </span>
+      </div>
     </div>
   )
 }
