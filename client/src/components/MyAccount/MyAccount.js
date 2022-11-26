b99dc221560cf75ac623d507beb95418553971c4
Pete Fowler <fowler.pete@gmail.com>
2022-11-26 13:33:15 -0700

    Complete unstar in MyAccount

--- client/src/components/MyAccount/MyAccount.js
+++ client/src/components/MyAccount/MyAccount.js
@@ -1,20 +1,18 @@
 import styles from "./MyAccount.module.css";
+import StarredListing from "./StarredListing";
 import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
-import useStar from "../../Hooks/useStar";
 import useHidden from "../../Hooks/useHidden";
 
 export default function MyAccount({ user, newUser }) {
   const [errors, setErrors] = useState([]);
   const [posts, setPosts] = useState([]);
-  const [starredPosts, setStarredPosts] = useState([]);
+  const [starred, setStarred] = useState([]);
   const [city, setCity] = useState([]);
   const [hiddens, setHiddens] = useState([]);
 
   const navigate = useNavigate();
-
-  const { starred, checkIfStarred, handleStarClick } = useStar();
 
   // Fetch posts
   useEffect(() => {
@@ -39,7 +37,7 @@ export default function MyAccount({ user, newUser }) {
     if (user !== null) {
       fetch(`/posts?starred=${user.starred}`).then((r) => {
         if (r.ok) {
-          r.json().then((data) => setStarredPosts(data));
+          r.json().then((data) => setStarred(data));
         } else {
           r.json().then((err) => setErrors(err.error));
         }
@@ -172,19 +170,13 @@ export default function MyAccount({ user, newUser }) {
           <div className={`${styles.date} ${styles.heading}`}>posted date</div>
         </div>
         {starred.map((post) => (
-          <div key={post.id} className={styles.starred}>
-            <div className={styles.manage}>
-              <button onClick={() => showPost(post.id)}>display</button>
-              <button>un-star</button>
-            </div>
-            <div className={styles.title}>{post.title}</div>
-            <div className={styles.area}>
-              <b>{post.area}</b> {post.category}
-            </div>
-            <div className={styles.date}>
-              {format(new Date(post.created_at), "dd MMM yyyy k:mm")}
-            </div>
-          </div>
+          <StarredListing
+            key={post.id}
+            post={post}
+            showPost={showPost}
+            user={user}
+            newUser={newUser}
+          />
         ))}
       </div>
       <br />
